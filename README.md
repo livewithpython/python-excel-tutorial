7d8bd36b17011b0c0a3f8740ff14e7fa3a89f8c4
Russel Mahmud <hossain@newscredc.om>
2014-02-01 09:03:18 +0600

    initial lecture design

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-python-excel-tutorial
+Python Excel Tutorial
 =====================
+
+How to install
+===============
+1. Install python
+2. pip install ipython pyzmq jinja2 tornado pandas xlrd openpyxl xlwt matplotlib
---
+++ data/adder (1).py
@@ -0,0 +1,28 @@
+# Max Shron Data Strategy for BRAC
+# Proof of concept: adding cells across sheets
+
+import xlrd, xlwt
+
+INPUT1 = 'input1.xlsx'
+INPUT2 = 'input2.xlsx'
+OUTPUT = 'output.xlsx'
+
+wb1 = xlrd.open_workbook(INPUT1)
+output = xlwt.Workbook()
+
+s1 = output.add_sheet('First type')
+sum1, sum2, sum3 = 0, 0, 0
+for sheet in wb1.sheets():
+    sum1 += sheet.cell(3,2).value
+    sum2 += sheet.cell(4,2).value
+    sum3 += sheet.cell(5,2).value
+s1.write(3, 2, sum1)
+s1.write(4, 2, sum2)
+s1.write(5, 2, sum3)
+    
+# Copy headers, etc
+# 1,1
+# 1,2
+# 0,[3-5]
+
+output.save('output.xls')
